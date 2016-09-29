12082d7ec7a62cbce2b18a386215632355c40a39
Rocknrolla91 <lifar_tut_net@mail.ru>
2016-09-29 14:14:23 +0300

    Adjacency Lists representation of a Graph is implemented.

--- graphs/representations/adjacency_lists/include/adjacency_lists.hpp
+++ graphs/representations/adjacency_lists/include/adjacency_lists.hpp
@@ -12,7 +12,7 @@
 
 /*
  Graph representations:
- 	Array of Adjacency List for each vertex
+ 	Array of Adjacency List for each vertex for unweighted undirected graph
  	a -- b
  	|    |
  	c    d
@@ -29,14 +29,65 @@ public:
 	int add_vertex(T); // returns an id of the vertex (position in vertices[])
 	void add_edge(int, int); // positions (ids) of vertices
 	void remove_edge(int, int);
-	Linked_List<T> * neighbours(int); // adjacent vertices for a given vertex id
+	LinkedList<T> neighbours(int); // adjacent vertices for a given vertex id
 	void display_lists();
 private:
 	std::vector<T> vertices;
-	std::vector<Linked_List<T>> adj;
-}
+	std::vector<LinkedList<T>> adj;	
+};
 
 template <class T>
 int Graph<T>::add_vertex(T new_data){
-	
+	vertices.push_back(new_data);
+	int index = vertices.size()-1;
+	LinkedList<T> adj_list;
+	adj_list.push(new_data);
+	adj.push_back(adj_list);
+	return index;
+}
+
+template <class T>
+void Graph<T>::add_edge(int index1, int index2){
+	if(index1 >= vertices.size() || index2 >= vertices.size()){
+		return;
+	}
+
+	T v1 = vertices[index1];
+	T v2 = vertices[index2];
+
+	adj[index1].append(v2);
+	adj[index2].append(v1);
+}
+
+template <class T>
+void Graph<T>::remove_edge(int index1, int index2){
+	if(index1 >= vertices.size() || index2 >= vertices.size()){
+		return;
+	}
+
+	T v1 = vertices[index1];
+	T v2 = vertices[index2];
+
+	adj[index1].remove(v2);
+	adj[index2].remove(v1);
+}
+
+template <class T>
+LinkedList<T> Graph<T>::neighbours(int index){
+	if(index >= vertices.size()){
+		return 0;
+	}
+
+	LinkedList<T> list = adj[index];
+	list.remove_head();
+	return list;
+
+}
+
+template <class T>
+void Graph<T>::display_lists(){
+	for(auto it: adj){
+		it.print_list();
+		std::cout << std::endl;
+	}
 }
--- graphs/representations/adjacency_lists/main.cpp
+++ graphs/representations/adjacency_lists/main.cpp
@@ -1,7 +1,32 @@
+//  
+//   main.cpp   
+//   graph representations  
+//  
+//   Created by alifar on 9/28/16.
+//   Copyright © 2016 alifar. All rights reserved.
 //
-//  main.cpp
-//  graph representations
-//
-//  Created by alifar on 9/28/16.
-//  Copyright © 2016 alifar. All rights reserved.
-//
+
+#include "adjacency_lists.hpp"
+
+using namespace std;
+
+int main(int argc, const char *argv[]){
+	Graph<int> *graph = new Graph<int>();
+	cout << graph->add_vertex(4) << endl;
+	cout << graph->add_vertex(1) << endl;
+	cout << graph->add_vertex(3) << endl;
+
+	graph->add_edge(0, 1);
+	graph->add_edge(0, 2);
+	graph->add_edge(1, 2);
+
+	cout << endl;
+	graph->display_lists();
+
+	graph->remove_edge(0, 2);
+
+	cout << endl;
+
+	graph->display_lists();
+
+}
