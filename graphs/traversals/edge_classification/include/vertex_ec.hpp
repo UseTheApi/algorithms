85486bdcb589b8b2ac6e56241319623e40d008d8
Alena Lifar <lifar_tut_net@mail.ru>
2016-10-14 08:52:22 +0300

    EdgeClassification vertex inherits from VertexBfs

--- graphs/traversals/edge_classification/include/vertex_ec.hpp
+++ graphs/traversals/edge_classification/include/vertex_ec.hpp
@@ -6,21 +6,27 @@
 //  Copyright © 2016 alifar. All rights reserved.
 //
 
-// #include <vertex_bfs.hpp>
-
+#include <vertex_bfs.hpp>
 #include <map>
 #include <iostream>
 
 enum EdgeType { kTree, kForward, kBackward, kCross };
 
-class Test{
-private:
-	std::map<char, EdgeType> types_;
+template <class T>
+class VertexEc: public VertexBfs<T>{
 public:
-	void add_type(char a, EdgeType type){
-		types_[a] = type;
-	}
-	std::map<char, EdgeType> get_types(){
-		return types_;
-	}
+	void add_type(VertexEc<T> *, EdgeType);
+	std::map<VertexEc<T> *, EdgeType> get_types();
+private:
+	std::map<VertexEc<T> *, EdgeType> edge_types_;
 };
+
+template <class T>
+void VertexEc<T>::add_type(VertexEc<T> *vertex, EdgeType ktype){
+	edge_types_[vertex] = ktype;
+}
+
+template <class T>
+std::map<VertexEc<T> *, EdgeType> get_types(){
+	return edge_types_;
+}
