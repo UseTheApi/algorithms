dad6aea27374765403ab9f667144fe179b864b56
Alena Lifar <lifar_tut_net@mail.ru>
2016-10-19 13:35:02 +0300

    new implementation of queue using Linked List with cmake build and template

--- stack_and_queue/queue_linked_list/include/queue_linked_list.hpp
+++ stack_and_queue/queue_linked_list/include/queue_linked_list.hpp
@@ -24,7 +24,7 @@ public:
 	void set_next(Queue<T> *);
 
 	template <class Q>
-	friend std::ostream & operator<<(std::ostream &os, const Queue<T> *node);
+	friend std::ostream & operator<<(std::ostream &os, const Queue<Q> *node);
 	// This part to allow foreach loop for this DS
 	typedef T* iterator;
 	typedef const T*  const_iterator;
@@ -36,6 +36,7 @@ private:
 	T data_;
 	Queue<T> *next_;
 	Queue<T> *head_;
+	Queue<T> *rear_;
 	size_t size_;
 	std::vector<T> store_;
 };
@@ -44,6 +45,7 @@ template <class T>
 Queue<T>::Queue(){
 	next_ = 0;
 	head_ = 0;
+	rear_ = 0;
 	size_ = 0;
 }
 
@@ -52,8 +54,70 @@ Queue<T>::Queue(T init_data){
 	data_ = init_data;
 	next_ = 0;
 	head_ = 0;
+	rear_ = 0;
 	size_ = 1;
 }
 
+template <class T>
+T Queue<T>::get_data(){
+	return data_;
+}
 
+template <class T>
+void Queue<T>::set_data(T new_data){
+	data_ = new_data;
+}
 
+template <class T>
+Queue<T> * Queue<T>::get_next(){
+	return next_;
+}
+
+template <class T>
+void Queue<T>::set_next(Queue<T> *new_next){
+	next_ = new_next;
+}
+
+template <class T>
+void Queue<T>::Enqueue(T new_data){
+	++size_;
+	store_.push_back(new_data);
+	if(!head_){
+		head_ = rear_ = new Queue<T>(new_data);
+		return;
+	}
+	Queue<T> *new_node = new Queue<T>(new_data);
+	rear_->next_ = new_node;
+	rear_ = new_node;
+}
+
+template <class T>
+void Queue<T>::Dequeue(){
+	if(!head_){
+		return;
+	}
+	--size_;
+	store_.pop_back();
+	Queue<T> *tmp = head_;
+	delete head_;
+	head_ = tmp->next_;
+	if(!head_){
+		rear_ = 0;
+	}
+}
+
+template <class T>
+Queue<T> * Queue<T>::Top(){
+	return head_;
+}
+
+template <class T>
+bool Queue<T>::empty(){
+	return !head_;
+}
+
+template <class Q>
+std::ostream & operator<<(std::ostream &os, const Queue<Q> *node){
+	os << node->data_;
+	return os;
+}
--- stack_and_queue/queue_linked_list/main.cpp
+++ stack_and_queue/queue_linked_list/main.cpp
@@ -12,5 +12,42 @@
 using namespace std;
 
 int main(int argc, const char *argv[]){
+	cout << "Queue implemented with Linked List" << endl;
+	Queue<int> queue;
+	int item;
+	cout << "Add items to Queue (press Enter when done): ";
+	do{
+		cin >> item;
+		queue.Enqueue(item);
+	} while(cin.get() != '\n');
+
+	cout << "ForEach loop on Queue: ";
+	for(auto it: queue){
+		cout << it << " ";
+	}
+	cout << endl;
+
+	cout << "Dequeue 1 item from Queue" << endl;
+	queue.Dequeue();
+	cout << "Top of Queue: " << queue.Top() << endl;
+
+	cout << "Add some more items to Queue (press Enter when done): ";
+	do{
+		cin >> item;
+		queue.Enqueue(item);
+	} while(cin.get() != '\n');
+
+	cout << "ForEach loop on Queue: ";
+	for(auto it: queue){
+		cout << it << " ";
+	}
+	cout << endl;
+
+	cout << "Dequeue items until Empty: ";
+	while(!queue.empty()){
+		queue.Dequeue();
+	} 
+	cout << endl;
+
 	return 0;
 }
