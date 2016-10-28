86fbe8329544a40d9354194a179e2dbc65a34ca5
Alena Lifar <lifar_tut_net@mail.ru>
2016-10-28 15:25:43 +0300

    [INIT] initial files for BST refactor

---
+++ trees/binary_search_tree/implementation/include/binary_search_tree.hpp
@@ -0,0 +1,9 @@
+//
+//  binary_search_tree.hpp
+//  algorithms. BST implementation.
+//
+//  Created by alifar on 10/28/16.
+//  Copyright © 2016 alifar. All rights reserved.
+//
+
+
---
+++ trees/binary_search_tree/implementation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms. BST implementation.
+//
+//  Created by alifar on 10/28/16.
+//  Copyright © 2016 alifar. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]){
+	return 0;
+}
