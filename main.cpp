1ae62ae617f9e27fa0683efb1c0d1a65da9fcc68
Alena Lifar <lifar_tut_net@mail.ru>
2016-11-27 00:55:26 +0300

    [DELETE] old implementations for ancestor and successor in a tree

--- main.cpp
+++ main.cpp
@@ -16,22 +16,6 @@
 
 
 int main(int argc, const char * argv[]) {
-
-// TEST: Lowest Common Ancestor: lowestCommonAncestor.hpp
-//    Lca *tree = new Lca();
-//    tree->setRoot(tree->insert(tree->getRoot(), 6));
-//    tree->setRoot(tree->insert(tree->getRoot(), 7));
-//    tree->setRoot(tree->insert(tree->getRoot(), 10));
-//    tree->setRoot(tree->insert(tree->getRoot(), 3));
-//    tree->setRoot(tree->insert(tree->getRoot(), 4));
-//    tree->setRoot(tree->insert(tree->getRoot(), 5));
-//    tree->setRoot(tree->insert(tree->getRoot(), 2));
-//    
-//    BstNodeWithParent *n1 = tree->search(tree->getRoot(), 2);
-//    BstNodeWithParent *n2 = tree->search(tree->getRoot(), 5);
-//    
-//    std::cout << tree->lowestCommonAncesrotRoot(tree->getRoot(), n1, n2)->data << std::endl;
-//    std::cout << tree->lowestCommonAncestorParent(n1, n2)->data << std::endl;
    
 // TEST: check if trees are equal: equalTree.hpp (+ binarySearchTree.hpp)
 //    Bst *tree1 = new Bst();
@@ -70,89 +54,5 @@ int main(int argc, const char * argv[]) {
 //    }
 //    std::cout << std::endl;
 
-// TEST: In Order Successor in BST: bstInorderSuccessor.hpp
-//    bstSuccessor *tree = new bstSuccessor();
-//    tree->setRoot(tree->insert(tree->getRoot(), 5));
-//    tree->setRoot(tree->insert(tree->getRoot(), 7));
-//    tree->setRoot(tree->insert(tree->getRoot(), 10));
-//    tree->setRoot(tree->insert(tree->getRoot(), 3));
-//    tree->setRoot(tree->insert(tree->getRoot(), 4));
-//    BstNodeWithParent *temp = tree->getRoot()->left->right;
-//    std::cout << tree->findSuccessorByParent(temp)->data << std::endl;
-//    std::cout << tree->findSuccessorByRoot(tree->getRoot(), temp)->data << std::endl;
-
-// TEST: deletion from BST: bstTraversals.hpp or binarySearchTree.hpp
-//    Bst *tree = new Bst();
-//    tree->insert(4);
-//    tree->insert(6);
-//    tree->insert(2);
-//    tree->insert(5);
-//    tree->insert(8);
-//    tree->insert(3);
-//    tree->insert(1);
-//    std::cout << "Current root:" << tree->getRoot()->data << std::endl;
-//    std::cout << "In Order Traversal:" << std::endl;
-//    inOrderTraversal(tree->getRoot());
-//    std::cout << std::endl;
-//    tree->setRoot(tree->remove(4));
-//    std::cout << "Current root:" << tree->getRoot()->data << std::endl;
-//    std::cout << "In Order Traversal:" << std::endl;
-//    inOrderTraversal(tree->getRoot());
-//    std::cout << std::endl;
-//    tree->setRoot(tree->remove(2));
-//    std::cout << "Current root:" << tree->getRoot()->data << std::endl;
-//    std::cout << "In Order Traversal:" << std::endl;
-//    inOrderTraversal(tree->getRoot());
-//    std::cout << std::endl;
-//    tree->setRoot(tree->remove(3));
-//    std::cout << "Current root:" << tree->getRoot()->data << std::endl;
-//    std::cout << "In Order Traversal:" << std::endl;
-//    inOrderTraversal(tree->getRoot());
-//    std::cout << std::endl;
-    
-// TEST: BST insertion, search for key, find Min, find min in a subtree: binarySearchTree.hpp or bstTraversals.hpp
-//    Bst *tree = new Bst();
-//    tree->insert(4);
-//    tree->insert(6);
-//    tree->insert(2);
-//    tree->insert(5);
-//    tree->insert(8);
-//    tree->insert(3);
-//    tree->insert(1);
-//    std::cout << tree->search(3)->data << std::endl;
-//    std::cout << tree->findMin()->data << std::endl;
-//    std::cout << tree->findSubtreeMin(tree->search(6))->data << std::endl;
-    
-// TEST: BST Traversals: bstTraversals.hpp
-//    Bst *tree = new Bst();
-//    tree->insert(4);
-//    tree->insert(6);
-//    tree->insert(2);
-//    tree->insert(5);
-//    tree->insert(8);
-//    tree->insert(3);
-//    tree->insert(1);
-//    std::cout << "Pre Order Traversal:" << std::endl;
-//    preOrderTraversal(tree->getRoot());
-//    std::cout << std::endl;
-//    std::cout << "In Order Traversal:" << std::endl;
-//    inOrderTraversal(tree->getRoot());
-//    std::cout << std::endl;
-//    std::cout << "Post Order Traversal:" << std::endl;
-//    postOrderTraversal(tree->getRoot());
-//    std::cout << std::endl;
-//    std::cout << "In Order Traversal using Stack:" << std::endl;
-//    inOrderTraversalStack(tree->getRoot());
-//    std::cout << std::endl;
-//    std::cout << "In Order Traversal Morris:" << std::endl;
-//    inOrderTraversalNoRecursion(tree->getRoot());
-//    std::cout << std::endl;
-//    std::cout << "Breadth First Traversal using Queue:" << std::endl;
-//    breadthFirstTraversalQueue(tree->getRoot());
-//    std::cout << std::endl;
-//    std::cout << "Breadth First Traversal using Height:" << std::endl;
-//    breadthFirstTraversalHeight(tree->getRoot());
-//    std::cout << std::endl;
- 
     return 0;
 }
