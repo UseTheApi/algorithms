d33d904abb3e0199d92fa249662caac1e6c657fc
usetheapi <lifar_tut_net@mail.ru>
2017-01-23 17:59:31 +0300

    [NEW] alien alphabet graph task init

---
+++ graphs/application/graph_tasks/alien_alphabet/include/alien_alphabet.hpp
@@ -0,0 +1,9 @@
+//
+//  alien_alphabet.hpp
+//  graphs. graph application tasks
+//
+//  Created by alifar on 01/23/17.
+//  Copyright © 2016 alifar. All rights reserved.
+//
+
+
---
+++ graphs/application/graph_tasks/alien_alphabet/main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  graphs. graph application tasks. alien alphabet
+//
+//  Created by alifar on 01/23/17.
+//  Copyright © 2016 alifar. All rights reserved.
+//
+
