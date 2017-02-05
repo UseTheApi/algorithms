faa5d5e82d0d7f6ac9dde3a945f1579173d45820
usetheapi <lifar_tut_net@mail.ru>
2017-02-05 16:54:12 +0300

    [INIT] biggest inclusive interval

---
+++ biggest_interval/main.cpp
@@ -0,0 +1,7 @@
+//
+//  main.cpp
+//  algorithms. biggest inclusive interval
+//
+//  Created by alifar on 02/05/17.
+//  Copyright © 2016 alifar. All rights reserved.
+//
---
+++ biggest_interval/src/biggest_interval.cpp
@@ -0,0 +1,7 @@
+//
+//  biggest_interval.cpp
+//  algorithms. biggest_interval
+//
+//  Created by alifar on 02/05/17.
+//  Copyright © 2016 alifar. All rights reserved.
+//
