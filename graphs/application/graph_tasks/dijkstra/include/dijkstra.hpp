eb394c1147b2e231adb98e3b44ba9390cf264030
usetheapi <lifar_tut_net@mail.ru>
2017-03-06 21:32:34 +0300

    [ADD] initial files for dijkstra algorithm implementation

---
+++ graphs/application/graph_tasks/dijkstra/include/dijkstra.hpp
@@ -0,0 +1,10 @@
+//
+//  dijkstra.hpp
+//  graphs. graph application tasks. dijkstra algorithm implementation
+//
+//  Created by alifar on 03/05/17.
+//  Copyright © 2017 alifar. All rights reserved.
+//
+
+
+
---
+++ graphs/application/graph_tasks/dijkstra/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  graphs. graph application tasks.dijkstra 
+//
+//  Created by alifar on 03/05/17.
+//  Copyright © 2017 alifar. All rights reserved.
+//
+
+#include "dijkstra.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]){
+    return 0;
+}
+
