060386904c0d2d1a898a870275e8f9394b8c8ca5
usetheapi <lifar_tut_net@mail.ru>
2017-03-08 21:42:31 +0300

    [ADD] Dijkstra algorithm implementation
    this implementation calculates min_weights for shortest paths for each vertices

--- graphs/application/graph_tasks/dijkstra/include/dijkstra.hpp
+++ graphs/application/graph_tasks/dijkstra/include/dijkstra.hpp
@@ -6,5 +6,60 @@
 //  Copyright © 2017 alifar. All rights reserved.
 //
 
+#include <weighted_d_graph.hpp>
+#include <queue>
+#include <iostream>
 
+template <class T>
+struct GreaterThenByPriority
+{
+  bool operator()(const VertexW<T>* lhs, const VertexW<T>* rhs) const
+  {
+    return lhs->min_distance > rhs->min_distance;
+  }
+};
+
+template <class T>
+using PriorityQueue = std::priority_queue<VertexW<T> *, std::vector<VertexW<T>*>, GreaterThenByPriority<T>>;
+
+template <class T>
+void InitializeQueue(PriorityQueue<T> &q, WeightedDGraph<T> &graph, T source){
+    std::pair<VertexW<T> *, int> source_pair= graph.get_vertex(source);
+    source_pair.first->min_distance = 0;
+    for(auto it: graph.get_vertices()){
+        if(it->data != source){
+            it->min_distance = INT_MAX;
+        } else{
+            it->min_distance = 0;
+        }
+        std::cout << it << " " << it->min_distance << std::endl;
+        q.push(it);
+    }
+}
+
+template <class T>
+void DisplayMinWeights(WeightedDGraph<T> &graph){
+    std::cout << "Min Weights for Vertices: " << std::endl;
+    for(auto it: graph.get_vertices()){
+        std::cout << it << ": " << it->min_distance << "; ";
+    }
+}
+
+template <class T>
+void ShortestPathsDijkstra(WeightedDGraph<T> &graph, T source){
+    PriorityQueue<T> q;
+    InitializeQueue(q, graph, source);
+    while(!q.empty()){
+        VertexW<T> *cur_v = q.top();
+        q.pop();
+        std::vector<std::pair<VertexW<T> *, int>> cur_neighbours = graph.Neighbours(cur_v->data);
+        for(auto it: graph.Neighbours(cur_v->data)){
+            if(it.first->min_distance > it.second + cur_v->min_distance){
+                it.first->min_distance = it.second + cur_v->min_distance;
+            }
+        }
+    }
+    DisplayMinWeights(graph);
+    return;
+}
 
--- graphs/application/graph_tasks/dijkstra/main.cpp
+++ graphs/application/graph_tasks/dijkstra/main.cpp
@@ -7,11 +7,39 @@
 //
 
 #include "dijkstra.hpp"
-#include <iostream>
 
 using namespace std;
 
 int main(int argc, const char *argv[]){
+    cout << endl;
+    cout << "Graph Shortest Paths: Weighted Graph." << endl;
+    WeightedDGraph<char> graph;
+    cout << "Add vertices to the Graph (type - char): ";
+    char item;
+
+    do{
+        cin >> item;
+        graph.AddVertex(item);
+    } while(cin.get() != '\n');
+
+    cout << "Add edges to the graph by entering vertex data and integer weight (Press enter when done): " << endl;
+    int weight;
+    char v1, v2;
+    do{
+        cin >> v1;
+        cin >> v2;
+        cin >> weight;
+        graph.AddEdge(v1, v2, weight);
+    } while(cin.get() != '\n');
+    cout << endl;
+    cout << "Adjacency Lists for the graph: " << endl;
+    graph.display_lists();
+    cout << endl;
+    cout << "Enter a Source vertex for Dijkstra: ";
+    char source_item;
+    cin >> source_item;
+    ShortestPathsDijkstra(graph, source_item);
+    cout << endl;
     return 0;
 }
 
