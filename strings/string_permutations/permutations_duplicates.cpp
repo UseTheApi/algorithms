14a0bf3ad2f0965d7152497f7548a488a18c8b69
usetheapi <lifar_tut_net@mail.ru>
2017-03-09 16:17:44 +0300

    [FIX] tabs and formatting

--- strings/string_permutations/permutations_duplicates.cpp
+++ strings/string_permutations/permutations_duplicates.cpp
@@ -9,26 +9,26 @@
 
 /*
  Write a program to print all permutations of a given string.
- 						ABC
- 			ABC			BAC		    CBA
- 		ABC	   ACB   BAC  BCA    CBA   CAB
+                        ABC
+            ABC         BAC         CBA
+        ABC    ACB   BAC  BCA    CBA   CAB
 */
 
 using namespace std;
 
 void swap(char &ch1, char &ch2){
-	char tmp = ch1;
-	ch1 = ch2;
-	ch2 = tmp;
+    char tmp = ch1;
+    ch1 = ch2;
+    ch2 = tmp;
 }
 
 void permute_with_duplicates(string &str, int start, int end){
-	if(start == end){
-		cout << str << endl;
-	}
-	for(int i = start; i <= end; ++i){
-		swap(str[start], str[i]);
-		permute_with_duplicates(str, start+1, end);
-		swap(str[start], str[i]);
-	}
+    if(start == end){
+        cout << str << endl;
+    }
+    for(int i = start; i <= end; ++i){
+        swap(str[start], str[i]);
+        permute_with_duplicates(str, start+1, end);
+        swap(str[start], str[i]);
+    }
 }
